21c6705caa89fe316fdb6b14313dcda95d7f690e
Med-dev-sl <mohamedsallu24@gmail.com>
2026-06-22 10:00:37 +0000

    made changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
-      <body className="flex min-h-full flex-col">
+      <body className="flex min-h-full flex-col overflow-x-hidden">
         <LayoutContent>{children}</LayoutContent>
       </body>
     </html>
